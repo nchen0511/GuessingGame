b77fb290201dec73c145b2a107b778f10da4c13c
BT_1N3_30 <nchen0511@bths.edu>
2018-09-25 10:03:19 -0400

    done

--- src/aiGuess.java
+++ src/aiGuess.java
@@ -5,6 +5,35 @@ public class aiGuess {
         System.out.println("Hello, user, what should I refer to you as?");
         Scanner number = new Scanner(System.in);
         String name = number.next();
-        System.out.println("Hi, " + name + ". Let's play a guessing game. Pick a number between 1 to 100, I'l guess it!");
+        System.out.println("Hi, " + name + ". Let's play a guessing game. Pick a number between 1 to 100, I'll guess it!");
+        System.out.println("You have to tell me if my number is (g)reater or (l)esser than your number, or if I'm (c)orrect).");
+        System.out.println("Ready? Here I go!");
+        int count = 1;
+        int first = 0;
+        int last = 100;
+        while(true){
+            int avg = (int)(Math.ceil(((first+last)/2.0)));
+            System.out.println("Okay... is your number " + avg + " ?");
+            String response = number.next();
+            if (response.equals("c")){
+                break;
+            }
+            if(response.equals("g")){
+                count++;
+                first = avg;
+            } else if(response.equals("l")){
+                count++;
+                last = avg;
+            } else {
+                System.out.println("You have to tell me if my number is (g)reater or (l)esser than your number, or if I'm (c)orrect)!");
+            }
+            if(count>7){
+                System.out.println("Hmm... I think you were lying at some point, " + name + ".");
+                break;
+            }
+        }
+        if(count<8){
+        System.out.println("Nice! I got it! That only took me " + count + " tries, " + name + ".");
+        }
     }
 }
